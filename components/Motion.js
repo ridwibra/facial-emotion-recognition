676c70c13da9f841ea84d097c9a184285c9ba0aa
Ridwan <ridwibra@gmail.com>
2023-07-30 21:41:40 +0300

    Update app

--- components/Motion.js
+++ components/Motion.js
@@ -4,12 +4,13 @@ import * as blazeface from "@tensorflow-models/blazeface";
 import Webcam from "react-webcam";
 
 const emotions = {
-  0: { en: "anger", ar: "غضب" },
-  1: { en: "fear", ar: "خوف" },
-  2: { en: "happiness", ar: "سعادة" },
-  3: { en: "sadness", ar: "حزن" },
-  4: { en: "surprise", ar: "دهشة" },
-  5: { en: "neutral", ar: "محايد" },
+  0: { en: "Angry", ar: "غاضب" },
+  1: { en: "Disgust", ar: "اشمئزاز" },
+  2: { en: "Fear", ar: "خوف" },
+  3: { en: "Happy", ar: "سعيد" },
+  4: { en: "Neutral", ar: "محايد" },
+  5: { en: "Sad", ar: "حزين" },
+  6: { en: "Surprise", ar: "مفاجأة" },
 };
 
 function Motion() {
@@ -180,6 +181,192 @@ function Motion() {
 }
 
 export default Motion;
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// import React, { useRef, useState, useEffect } from "react";
+// import * as tf from "@tensorflow/tfjs";
+// import * as blazeface from "@tensorflow-models/blazeface";
+// import Webcam from "react-webcam";
+
+// const emotions = {
+//   0: { en: "anger", ar: "غضب" },
+//   1: { en: "fear", ar: "خوف" },
+//   2: { en: "happiness", ar: "سعادة" },
+//   3: { en: "sadness", ar: "حزن" },
+//   4: { en: "surprise", ar: "دهشة" },
+//   5: { en: "neutral", ar: "محايد" },
+// };
+
+// function Motion() {
+//   const webcamRef = useRef(null);
+//   const canvasRef = useRef(null);
+//   const [emotionModel, setEmotionModel] = useState(null);
+//   const [faceModel, setFaceModel] = useState(null);
+//   const [emotion, setEmotion] = useState(null);
+//   const [cameraOn, setCameraOn] = useState(false);
+
+//   // Load the models
+//   useEffect(() => {
+//     async function loadModels() {
+//       const emotionModel = await tf.loadLayersModel(
+//         "/tfjs_model_new/model.json"
+//       );
+//       setEmotionModel(emotionModel);
+//       const faceModel = await blazeface.load();
+//       setFaceModel(faceModel);
+//     }
+//     loadModels();
+//   }, []);
+
+//   // Run the models on the webcam feed
+//   useEffect(() => {
+//     let interval;
+//     if (emotionModel && faceModel && cameraOn) {
+//       interval = setInterval(() => {
+//         detect(emotionModel, faceModel);
+//       }, 10);
+//     }
+//     return () => clearInterval(interval);
+//   }, [emotionModel, faceModel, cameraOn]);
+
+//   const detect = async (emotionModel, faceModel) => {
+//     if (
+//       typeof webcamRef.current !== "undefined" &&
+//       webcamRef.current !== null &&
+//       webcamRef.current.video.readyState === 4
+//     ) {
+//       // Get video properties
+//       const video = webcamRef.current.video;
+//       const videoWidth = webcamRef.current.video.videoWidth;
+//       const videoHeight = webcamRef.current.video.videoHeight;
+
+//       // Set video width and height
+//       webcamRef.current.video.width = videoWidth;
+//       webcamRef.current.video.height = videoHeight;
+
+//       // Set canvas width and height
+//       canvasRef.current.width = videoWidth;
+//       canvasRef.current.height = videoHeight;
+
+//       // Detect faces using Blazeface
+//       let img = tf.browser.fromPixels(video);
+//       const predictions = await faceModel.estimateFaces(img);
+
+//       if (predictions.length > 0) {
+//         // Draw a bounding box around the detected face
+//         const ctx = canvasRef.current?.getContext("2d");
+//         if (ctx) {
+//           ctx.clearRect(
+//             0,
+//             0,
+//             canvasRef.current.width,
+//             canvasRef.current.height
+//           );
+//           ctx.strokeStyle = "red";
+//           ctx.lineWidth = 5;
+//           const start = predictions[0].topLeft;
+//           const end = predictions[0].bottomRight;
+//           const size = [end[0] - start[0], end[1] - start[1]];
+
+//           // Mirror the coordinates of the bounding box
+//           const mirroredStart = [videoWidth - start[0] - size[0], start[1]];
+//           ctx.strokeRect(mirroredStart[0], mirroredStart[1], size[0], size[1]);
+//         }
+
+//         // Preprocess the input image for emotion detection
+//         img = tf.image.resizeBilinear(img, [48, 48]);
+//         img = tf.mean(img, -1);
+//         img = img.expandDims(-1);
+
+//         // Make a prediction using the emotion model
+//         const prediction = emotionModel.predict(img.expandDims(0));
+
+//         // Process the prediction and update the state
+//         const emotionIndex = prediction.argMax(-1).dataSync()[0];
+//         setEmotion(emotions[emotionIndex]);
+//       } else {
+//         setEmotion({ en: "No face detected", ar: "لم يتم العثور على وجه" });
+//       }
+
+//       // Dispose the input image
+//       img.dispose();
+//     }
+//   };
+
+//   const handleCameraToggle = () => {
+//     setCameraOn(!cameraOn);
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+//       <div className="flex flex-col items-center">
+//         <h1 className="text-4xl font-bold mb-1">Real-time Video</h1>
+//         <button
+//           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1"
+//           onClick={handleCameraToggle}
+//         >
+//           {cameraOn ? "Turn Camera Off" : "Turn Camera On"}
+//         </button>
+//         <div className="flex items-center">
+//           {/* Display the results */}
+//           {emotion ? (
+//             <div className="mt-1">
+//               <p>
+//                 <span className="text-xl font-bold">Emotion: </span>{" "}
+//                 {emotion.en}--{emotion.ar}
+//               </p>
+//               <p className="text-2xl"></p>
+//               <p className="text-2xl"></p>
+//             </div>
+//           ) : (
+//             cameraOn && (
+//               <div>
+//                 <p>Please wait</p>
+//               </div>
+//             )
+//           )}
+//         </div>
+//         {cameraOn && (
+//           <>
+//             <Webcam
+//               ref={webcamRef}
+//               muted={true}
+//               className="mirror"
+//               style={{
+//                 width: 640,
+//                 height: 480,
+//               }}
+//             />
+//             <canvas
+//               ref={canvasRef}
+//               style={{
+//                 position: "absolute",
+//                 marginLeft: "auto",
+//                 marginRight: "auto",
+//                 left: 0,
+//                 right: 0,
+//                 textAlign: "center",
+//                 zIndex: 9,
+//                 width: 640,
+//                 height: 480,
+//               }}
+//             />
+//           </>
+//         )}
+
+//         {!cameraOn && (
+//           <div className="mt-4">
+//             <p className="text-xl font-bold">Camera is off</p>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Motion;
+
+/////////////////////////////////////////////////////////////////////////
 
 // // without face detection model
 // import React, { useRef, useState, useEffect } from "react";
--- components/Still.js
+++ components/Still.js
@@ -4,12 +4,13 @@ import * as blazeface from "@tensorflow-models/blazeface";
 import NextImage from "next/image";
 
 const emotions = {
-  0: { en: "anger", ar: "غضب" },
-  1: { en: "fear", ar: "خوف" },
-  2: { en: "happiness", ar: "سعادة" },
-  3: { en: "sadness", ar: "حزن" },
-  4: { en: "surprise", ar: "دهشة" },
-  5: { en: "neutral", ar: "محايد" },
+  0: { en: "Angry", ar: "غاضب" },
+  1: { en: "Disgust", ar: "اشمئزاز" },
+  2: { en: "Fear", ar: "خوف" },
+  3: { en: "Happy", ar: "سعيد" },
+  4: { en: "Neutral", ar: "محايد" },
+  5: { en: "Sad", ar: "حزين" },
+  6: { en: "Surprise", ar: "مفاجأة" },
 };
 
 function Still() {
@@ -47,10 +48,7 @@ function Still() {
       alert("File upload canceled");
       return;
     }
-    const acceptedExtensions = ["jpeg", "png", "jpg"];
-    const extension =
-      file && file.name ? file.name.split(".").pop().toLowerCase() : null;
-    if (file && model && acceptedExtensions.includes(extension)) {
+    if (file && model && file.type.startsWith("image/")) {
       setIsLoading(true);
       setImgSrc(URL.createObjectURL(file));
       const img = new Image();
@@ -72,8 +70,12 @@ function Still() {
           // Make a prediction using the model
           const emotionPredictions = await model.predict(tensor).dataSync();
 
+          // Get the predicted emotion in English and Arabic
+          const index = tf.argMax(emotionPredictions).dataSync()[0];
+          const predictedEmotion = emotions[index];
+
           // Update the state with the prediction result
-          setPred(emotions[tf.argMax(emotionPredictions).dataSync()[0]]);
+          setPred(predictedEmotion);
           setIsLoading(false);
           setEmotion(null);
         } else {
@@ -87,7 +89,7 @@ function Still() {
     } else {
       setImgSrc(null);
       setPred(null);
-      alert("Invalid file type. Please choose a JPEG, PNG, or JPG file.");
+      alert("Invalid file type. Please choose an image file.");
     }
   };
 
@@ -100,7 +102,7 @@ function Still() {
     <div className="h-3/4 w-screen p-6 bg-gray-100 flex flex-col items-center justify-center">
       <h1 className="text-3xl font-medium text-black mb-4">Image/Photo</h1>
       <p className="text-gray-500 mb-4">
-        Accepted file extensions: .jpeg, .png, .jpg
+        Accepted file types: images of any kind
       </p>
       <label
         htmlFor="fileInput"
@@ -111,7 +113,7 @@ function Still() {
       <input
         id="fileInput"
         type="file"
-        accept="image/jpeg, image/png, image/jpg"
+        accept="image/*"
         className="hidden"
         onChange={handleFileChange}
       />
@@ -149,6 +151,162 @@ function Still() {
 }
 
 export default Still;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// import React, { useState, useEffect } from "react";
+// import * as tf from "@tensorflow/tfjs";
+// import * as blazeface from "@tensorflow-models/blazeface";
+// import NextImage from "next/image";
+
+// const emotions = {
+//   0: { en: "Angry", ar: "غاضب" },
+//   1: { en: "Disgust", ar: "اشمئزاز" },
+//   2: { en: "Fear", ar: "خوف" },
+//   3: { en: "Happy", ar: "سعيد" },
+//   4: { en: "Neutral", ar: "محايد" },
+//   5: { en: "Sad", ar: "حزين" },
+//   6: { en: "Surprise", ar: "مفاجأة" },
+// };
+
+// function Still() {
+//   const [imgSrc, setImgSrc] = useState();
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [pred, setPred] = useState();
+//   const [model, setModel] = useState();
+//   const [faceModel, setFaceModel] = useState(null);
+//   const [emotion, setEmotion] = useState(null);
+
+//   async function loadModel() {
+//     // Set the backend to use the CPU
+//     tf.setBackend("cpu");
+//     const loadedModel = await tf.loadLayersModel("/tfjs_model_new/model.json");
+//     setModel(loadedModel);
+//   }
+
+//   useEffect(() => {
+//     loadModel();
+//   }, []);
+
+//   useEffect(() => {
+//     async function loadFaceModel() {
+//       const faceModel = await blazeface.load();
+//       setFaceModel(faceModel);
+//     }
+//     loadFaceModel();
+//   }, []);
+
+//   const handleFileChange = async (event) => {
+//     const file = event.target.files[0];
+//     // Check if the selected file is empty (canceled)
+//     if (!file) {
+//       // Display a message to the user
+//       alert("File upload canceled");
+//       return;
+//     }
+//     const acceptedExtensions = ["jpeg", "png", "jpg"];
+//     const extension =
+//       file && file.name ? file.name.split(".").pop().toLowerCase() : null;
+//     if (file && model && acceptedExtensions.includes(extension)) {
+//       setIsLoading(true);
+//       setImgSrc(URL.createObjectURL(file));
+//       const img = new Image();
+//       img.src = URL.createObjectURL(file);
+//       img.onload = async () => {
+//         // Detect faces using BlazeFace
+//         const predictions = await faceModel.estimateFaces(img);
+
+//         if (predictions.length > 0) {
+//           // Preprocess the image data
+//           const tensor = tf.browser
+//             .fromPixels(img)
+//             .resizeNearestNeighbor([48, 48])
+//             .mean(2)
+//             .toFloat()
+//             .expandDims()
+//             .expandDims(-1);
+
+//           // Make a prediction using the model
+//           const emotionPredictions = await model.predict(tensor).dataSync();
+
+//           // Update the state with the prediction result
+//           setPred(emotions[tf.argMax(emotionPredictions).dataSync()[0]]);
+//           setIsLoading(false);
+//           setEmotion(null);
+//         } else {
+//           setPred(null);
+//           setEmotion({
+//             en: "No face detected",
+//             ar: "لم يتم العثور على وجه",
+//           });
+//         }
+//       };
+//     } else {
+//       setImgSrc(null);
+//       setPred(null);
+//       alert("Invalid file type. Please choose a JPEG, PNG, or JPG file.");
+//     }
+//   };
+
+//   const handleClearImage = () => {
+//     setImgSrc(null);
+//     setPred(null);
+//   };
+
+//   return (
+//     <div className="h-3/4 w-screen p-6 bg-gray-100 flex flex-col items-center justify-center">
+//       <h1 className="text-3xl font-medium text-black mb-4">Image/Photo</h1>
+//       <p className="text-gray-500 mb-4">
+//         Accepted file extensions: .jpeg, .png, .jpg
+//       </p>
+//       <label
+//         htmlFor="fileInput"
+//         className="text-blue-500 hover:text-blue-800 cursor-pointer mb-4"
+//       >
+//         Choose an image
+//       </label>
+//       <input
+//         id="fileInput"
+//         type="file"
+//         accept="image/jpeg, image/png, image/jpg"
+//         className="hidden"
+//         onChange={handleFileChange}
+//       />
+//       {pred && (
+//         <p className="text-gray-700 mb-4">
+//           Prediction: {pred && `${pred.en} / ${pred.ar}`}
+//         </p>
+//       )}
+//       {emotion && (
+//         <p className="text-gray-700 mb-4">
+//           {emotion.en} / {emotion.ar}
+//         </p>
+//       )}
+//       {imgSrc && (
+//         <>
+//           <NextImage
+//             src={imgSrc}
+//             alt="Chosen image"
+//             width={300}
+//             height={300}
+//             objectFit="contain"
+//             className="rounded-lg mb-4"
+//           />
+//           <button
+//             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4"
+//             onClick={handleClearImage}
+//           >
+//             Clear Image
+//           </button>
+//         </>
+//       )}
+//       {/* {isLoading && <p className="text-gray-500 mb-4">Loading...</p>} */}
+//     </div>
+//   );
+// }
+
+// export default Still;
+
+/////////////////////////////////////////////////////////////////////////////
 
 // // // without face detection model
 // import React, { useState, useEffect } from "react";
